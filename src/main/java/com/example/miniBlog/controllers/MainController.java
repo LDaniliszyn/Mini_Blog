7a0f4569b4d55583b75dd933466fd5ed3f270636
LDaniliszyn <53149409+LDaniliszyn@users.noreply.github.com>
2022-12-15 19:54:41 +0100

    home styling

--- src/main/java/com/example/miniBlog/controllers/MainController.java
+++ src/main/java/com/example/miniBlog/controllers/MainController.java
@@ -10,10 +10,10 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 @RequiredArgsConstructor
 public class MainController {
-    final PostService postService;
+    private final PostService postService;
 
     @GetMapping(value = {"/home", "/", "/index"})
-    public String getHome(Model model) {
+    public String getHome(final Model model) {
         model.addAttribute("posts", postService.getPosts());
         return "home";
     }
--- src/main/java/com/example/miniBlog/services/PostService.java
+++ src/main/java/com/example/miniBlog/services/PostService.java
@@ -9,6 +9,7 @@ import com.example.miniBlog.model.reopsitory.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import javax.annotation.PostConstruct;
 import java.time.LocalDate;
 import java.util.List;
 import java.util.Optional;
@@ -19,6 +20,23 @@ public class PostService {
     private final UserRepository userRepository;
     private final PostRepository postRepository;
     private final UserContextService userContextService;
+
+    @PostConstruct
+    public void addDefaultPosts(){
+        PostEntity post1 = PostEntity.builder()
+                .title("test post nr 1")
+                .createDate(LocalDate.now())
+                .content("test post content")
+                .build();
+        postRepository.saveAndFlush(post1);
+
+        PostEntity post2 = PostEntity.builder()
+                .title("test post nr 2")
+                .createDate(LocalDate.now())
+                .content("test post content2")
+                .build();
+        postRepository.saveAndFlush(post2);
+    }
 
     public boolean publishNewPost(PostForm postForm) {
         final Optional<UserEntity> optionalUserEntity = userRepository.findByEmail(userContextService.getLoggedName());
